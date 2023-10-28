e1505547cd3df60016e9d1136e23ffaf93da9145
Swarup Lamichhane <lswarup1234@gmail.com>
2023-10-28 15:04:55 +0545

    Updated Button CSS

--- client/src/components/buttons/CustomButton.jsx
+++ client/src/components/buttons/CustomButton.jsx
@@ -12,10 +12,11 @@ const CustomButton = ({
       <button
         type={type}
         disabled={isDisabled}
-        className={`flex gap-3 button text-white px-8 py-2 bg-primary rounded-lg hover:bg-red-800 ${className}`}
+        className={`flex gap-3 button text-white px-8 py-2 bg-primary  rounded-lg hover:bg-red-800 ${className}`}
         style={{
           ...styles,
           background: `${isDisabled ? "#4B5563" : "#EF4343"}`,
+          fontWeight: 500,
         }}
         onClick={onClick}
       >
