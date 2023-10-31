8bf4427121194694b7060abbff6106b3701649b8
Swarup Lamichhane <lswarup1234@gmail.com>
2023-10-31 14:44:20 +0545

    Completed Add Ingredients section

--- client/src/components/dropdowns/ItemsDropdown.jsx
+++ client/src/components/dropdowns/ItemsDropdown.jsx
@@ -1,40 +1,90 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
+import React from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
-const ItemsDropdown = ({ name, items, onChange, value, label }) => {
+const Select = ({
+  label,
+  options,
+  value: selectedOption,
+  onChange: handelChange,
+}) => {
   return (
-    <Box sx={{ width: 250 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          name={name}
-          value={value}
-          label={label}
-          onChange={onChange}
-          sx={{
-            "& .MuiInputBase-input": {
-              // backgroundColor: "yourColorHere",
-              borderColor: "#4C2B21",
-            },
-            "& .MuiOutlinedInput-notchedOutline": {
-              borderColor: "#4C2B21", // Change this to the desired border color
-            },
-          }}
-        >
-          {items.map((item, index) => (
-            <MenuItem value={item} key={index}>
-              {item}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </Box>
+    <Listbox
+      as="div"
+      //className={className}
+      value={selectedOption}
+      onChange={(event) => {
+        console.log("enfant", event);
+        handelChange(event);
+      }}
+    >
+      {({ open }) => (
+        <>
+          {/*label && (
+            <Listbox.Label className="mb-1 text-sm font-medium text-blue-gray-500">
+              {label}
+            </Listbox.Label>
+          )*/}
+          <div className="relative mt-1">
+            <span className="inline-block w-full rounded-md shadow-sm">
+              <Listbox.Button className="cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left  transition ease-in-out duration-150">
+                <span className="block truncate">{selectedOption.label}</span>
+                <span className="absolute inset-y-0 right-0 flex items-center pr-2 ml-3 pointer-events-none">
+                  <MdKeyboardArrowDown />
+                </span>
+              </Listbox.Button>
+            </span>
+            <div className="absolute z-10 w-full mt-1 bg-white rounded-md shadow-lg mb-11">
+              {/* bottom-0 will open the select menu up & mb-11 will put the dropup above the select option */}
+              <Transition
+                show={open}
+                leave="transition duration-100 ease-in"
+                leaveFrom="transform opacity-100"
+                leaveTo="transform opacity-0"
+              >
+                <Listbox.Options
+                  static
+                  className="py-1 overflow-auto text-base rounded-md max-h-56 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                >
+                  {options.map((option) => {
+                    return (
+                      <Listbox.Option
+                        as={React.Fragment}
+                        key={option.id}
+                        value={option}
+                      >
+                        {({ active, selected }) => {
+                          return (
+                            <li
+                              className={`${
+                                active
+                                  ? "text-white bg-accent"
+                                  : "text-gray-900"
+                              } cursor-default select-none relative py-2 pl-3 pr-9`}
+                            >
+                              <div className="flex items-center">
+                                <span
+                                  className={`${
+                                    selected ? "font-semibold" : "font-normal"
+                                  } flex items-center  truncate`}
+                                >
+                                  {option.label}
+                                </span>
+                              </div>
+                            </li>
+                          );
+                        }}
+                      </Listbox.Option>
+                    );
+                  })}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </div>
+        </>
+      )}
+    </Listbox>
   );
 };
-export default ItemsDropdown;
+
+export default Select;
--- client/src/pages/adminInventory/contents/AddIngredients.jsx
+++ client/src/pages/adminInventory/contents/AddIngredients.jsx
@@ -1,11 +1,131 @@
-import { IngredientForm } from "../../../components";
+import { useState } from "react";
+import {
+  TextInput,
+  CustomButton,
+  TextArea,
+  ItemsDropdown,
+} from "../../../components";
+import { toastSuccess, toastError } from "../../../utils/toast";
+import { PostRequest } from "../../../services/httpRequest";
 
 const AddIngredients = () => {
+  const ingredientTypes = [
+    {
+      id: "1",
+      label: "Pizza Base",
+      value: "pizzaBase",
+    },
+    {
+      id: "2",
+      label: "Cheese",
+      value: "cheese",
+    },
+    {
+      id: "3",
+      label: "Sauce",
+      value: "sauce",
+    },
+    {
+      id: "4",
+      label: "Veggies",
+      value: "veggies",
+    },
+    {
+      id: "5",
+      label: "Meat",
+      value: "meat",
+    },
+  ];
+
+  const temp = ingredientTypes.find((ext) => ext.value);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    price: "",
+    type: "pizzaBase",
+    quantity: "",
+    description: "",
+  });
+
+  const [currentExtension, setCurrentExtension] = useState(temp);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const response = await PostRequest("/inventory/addIngredient", formData);
+      if (response.status === 201) {
+        toastSuccess(response.data.message);
+        setFormData({
+          name: "",
+          price: "",
+          type: "pizzaBase",
+          quantity: "",
+          description: "",
+        });
+        setCurrentExtension(ingredientTypes[0]);
+      }
+      console.log("response", response);
+    } catch (error) {
+      console.log(error);
+      toastError(error.response.data.message);
+    }
+  };
+
   return (
-    <div className="flex flex-col w-full gap-10">
-      <IngredientForm heading={"Pizza Base"} action={"Add Pizza Base"} />
-      <IngredientForm heading={"Sauce"} action={"Add Sauce"} />
-      <IngredientForm heading={"Cheese"} action={"Add Cheese"} />
+    <div className="flex flex-col w-full gap-10 items-center">
+      <h1 className="text-xl font-medium border-b-2 border-b-gray-200 mb-5 pb-2">
+        Add Ingredients
+      </h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+        <div className="flex gap-5">
+          <TextInput
+            type="text"
+            name="name"
+            label="Name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <TextInput
+            type="number"
+            name="price"
+            label="Price"
+            value={formData.price}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex gap-5 items-center">
+          <TextInput
+            type="number"
+            name="quantity"
+            label="Quantity"
+            value={formData.quantity}
+            onChange={handleChange}
+          />
+          <ItemsDropdown
+            options={ingredientTypes}
+            value={currentExtension}
+            onChange={(event) => {
+              setCurrentExtension(event);
+              setFormData({ ...formData, type: event.value });
+            }}
+          />
+          <span>{formData.type}</span>
+        </div>
+        <TextArea
+          name="description"
+          label="Description (Optional)"
+          value={formData.description}
+          onChange={handleChange}
+        />
+        <div className="flex justify-center mt-3">
+          <CustomButton title="Add Ingredient" />
+        </div>
+      </form>
     </div>
   );
 };
