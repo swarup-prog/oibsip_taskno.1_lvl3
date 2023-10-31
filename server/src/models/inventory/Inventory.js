5a0edb9b38b86ea57c43cc9b4ddb45565bb5e2f2
Swarup Lamichhane <lswarup1234@gmail.com>
2023-10-31 09:31:13 +0545

    Added inventory for ingredients management

---
+++ server/src/models/inventory/Inventory.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const inventorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+});
+
+const Inventory = mongoose.model("Inventory", inventorySchema);
+
+module.exports = { Inventory };
