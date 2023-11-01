f11486381ae56c03cd963bfe0e5cde26ddd121dd
Swarup Lamichhane <lswarup1234@gmail.com>
2023-11-01 13:37:20 +0545

    Custom pizza order UI

--- client/src/pages/user/kitchen.jsx
+++ client/src/pages/user/kitchen.jsx
@@ -10,13 +10,21 @@ const Kitchen = () => {
   const tab = localStorage.getItem("userActiveTab");
   const [activeTab, setActiveTab] = useState(tab ? tab : "Available Pizza");
 
+  const [order, setOrder] = useState({
+    pizzaBase: "",
+    cheese: "",
+    sauce: "",
+    veggies: "",
+    meat: "",
+  });
+
   const handleTabClick = (tabTitle) => {
     setActiveTab(tabTitle);
     localStorage.setItem("userActiveTab", tabTitle);
   };
 
   return (
-    <div className="pt-20 px-[245.6px] pb-5  flex min-h-screen bg-gray-100 gap-5">
+    <div className="pt-20 px-16 pb-5  flex min-h-screen bg-gray-100 gap-5">
       <aside className=" items-center flex-col gap-2 flex-initial w-80 dashboard-section">
         <Tab
           title={"Available Pizza"}
@@ -29,10 +37,22 @@ const Kitchen = () => {
           isActive={activeTab === "Make Custom Pizza"}
         />
       </aside>
-      <section className=" flex-1 dashboard-section w-full">
+      <section className=" flex-1 dashboard-section w-full h-[899px] overflow-y-auto ">
         {activeTab === "Available Pizza" && <AvailablePizza />}
         {activeTab === "Make Custom Pizza" && <MakePizza />}
       </section>
+      <aside className=" items-center flex-col gap-2 flex-initial w-96 dashboard-section">
+        <h1 className="text-xl font-medium border-b-2 border-b-gray-200 mb-5 pb-2">
+          Your Order
+        </h1>
+        <div className="flex flex-col bg-accent text-secondary p-4 w-full rounded-lg gap-3">
+          <span>Pizza Base: {order.pizzaBase}</span>
+          <span>Cheese: {order.cheese}</span>
+          <span>Sauce: {order.sauce}</span>
+          {order.veggies && <span>Veggies: {order.veggies}</span>}
+          {order.meat && <span>Meat: {order.meat}</span>}
+        </div>
+      </aside>
     </div>
   );
 };
